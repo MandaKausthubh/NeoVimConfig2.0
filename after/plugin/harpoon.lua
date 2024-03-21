75af3f0fcee3553c1287e372f7bd42909d1cf9b4
Manda Kausthubh <kausthubh.manda@gmail.com>
2024-03-21 14:10:57 +0530

    Clashing shortcuts <C-d>, which was mapped to Harpoon remove file, now is a vim motion

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -3,7 +3,7 @@ local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<C-a>", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-vim.keymap.set("n", "<C-d>", mark.rm_file)
+vim.keymap.set("n", "<C-d-f>", mark.rm_file)
 vim.keymap.set("n", "<C-S-r>", mark.clear_all)
 
 vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
