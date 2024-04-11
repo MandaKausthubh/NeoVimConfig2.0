71026fc3b8b350dcf8d860b06a43074fc3f344e7
Manda Kausthubh <kausthubh.manda@gmail.com>
2024-04-11 21:19:53 +0530

    Added location section in lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -20,7 +20,7 @@ require('lualine').setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename', 'hostname'},
+    lualine_c = {'filename', 'hostname', 'location'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
