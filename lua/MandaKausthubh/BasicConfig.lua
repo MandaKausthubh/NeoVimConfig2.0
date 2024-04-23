7e846ecc5fe7bbf3074759af825c4f2853098f67
Manda Kausthubh <kausthubh.manda@gmail.com>
2024-04-23 13:29:15 +0530

    Adding the numbers and relative numbers

--- lua/MandaKausthubh/BasicConfig.lua
+++ lua/MandaKausthubh/BasicConfig.lua
@@ -14,6 +14,7 @@ vim.opt.shiftwidth = 4
 vim.opt.shiftround = true
 vim.opt.expandtab = true
 vim.opt.relativenumber = true
+vim.opt.number = true
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.keymap.set('v', '<C-l>', ":'<,'>!xclip -f -sel clip<CR>")
